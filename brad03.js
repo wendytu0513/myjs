6a809a29825481fe44083dbfa203f851a953b9c5
Chien <stdnt25@gmail.com>
2019-06-17 13:59:50 +0800

    init

---
+++ brad03.js
@@ -0,0 +1,30 @@
+let var1;
+let var2 = 123;
+let $$$ = 100;
+let $_$ = 123;
+document.write($_$ + "<br>");
+document.write(var1 + "<br>");
+var1 = 123;
+document.write(typeof(var1) + "<br>");
+var1 = 12.3;
+document.write(typeof(var1) + "<br>");
+var1 = true;
+document.write(typeof(var1) + "<br>");
+var1 = 'brad';
+document.write(typeof(var1) + "<br>");
+
+
+const name = 'brad';
+// name = "peter";
+//常數不可進行指派
+
+
+document.write("my name is " + name + "<br>");
+document.write(`my name is ${name}` + "<br>")
+let a = 10,b = 3;
+document.write(`${a} +${b} = ${a+b}`);
+
+
+let var1 = 10,var2 = 3;
+let var3 = var1 % var2;
+document.write(`${var1} / ${var2} = ${var3}`);
---
+++ brad04.js
@@ -0,0 +1,2 @@
+let a = 10,b = 3;
+document.write(`${a} +${b} = ${a+b}`);
