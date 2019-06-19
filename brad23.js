3d8acd214c74800a1796c41258fe6f666eefb194
Chien <stdnt25@gmail.com>
2019-06-19 09:00:48 +0800

    驗證身分證

---
+++ brad23.js
@@ -0,0 +1,13 @@
+function checktwid(id){
+    let ret = false;
+
+    if (id.length == 10){
+        let c1 = id.substr(0,1);
+        let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (letters.indexOf(c1)>=0){
+            let word = id.substr(0,2);
+            
+        }
+    }
+return ret;
+}
