84084b758f2193ae74b4f020f1d5ff9c5175be92
Chien <stdnt25@gmail.com>
2019-06-21 18:01:53 +0800

    20190621

--- brad36_api.js
+++ brad36_api.js
@@ -1,10 +1,24 @@
 function clone(source){
-    if (typeof(source) != 'object') return null;
+    if (source ==null || typeof(source) != 'object') 
+    return null;
 
     let target = new Object();
     for (let attr in source){
-        target[attr] = source[attr];
+        if (typeof source[attr] != 'object'){
+            target[attr] = source[attr];
+        }else{
+            target[attr] = clone(source[attr]);
+        }
+        
     }
     return target;
+}
 
+
+
+
+Date.prototype.getcheak = function(){
+    let w = this.getDay();
+    let ws = ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'];
+    return ws[w];
 }
